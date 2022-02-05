90198ed161f1ba9d899d8b0f3a822889af9bdbbb
jyw2671 <yjung@student.42seoul.kr>
2022-02-05 14:12:08 +0900

    fix main.cpp code

--- module06/ex02/main.cpp
+++ module06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yjung <yjung@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 11:53:40 by yjung             #+#    #+#             */
-/*   Updated: 2022/02/05 13:51:12 by yjung            ###   ########.fr       */
+/*   Updated: 2022/02/05 14:09:28 by yjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 int	main()
 {
-	srand(time(NULL));
-	for (int i = 0 ; i < 3 ; ++i)
 	{
-		Base* base = generate();
-		identify(base);
-		identify(*base);
-		std::cout << std::endl;
-		delete base;
+		srand(time(NULL));
+		for (int i = 0 ; i < 3 ; ++i)
+		{
+			Base* base = generate();
+			identify(base);
+			identify(*base);
+			std::cout << std::endl;
+			delete base;
+		}
 	}
 	// system("leaks ex02");
 	return (0);
